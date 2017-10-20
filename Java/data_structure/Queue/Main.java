515eb55397cd0b802425f7bf49a201f1aff3ffa9
nnks <ntomoya1010@gmail.com>
2017-10-20 16:49:01 +0900

    refactoring

--- Java/data_structure/Queue/Main.java
+++ Java/data_structure/Queue/Main.java
@@ -1,8 +1,15 @@
+/**
+ *
+ * アリ本P.37 迷路の最短路の回答
+ *
+ **/
+
 import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
+    // y, x座標を扱う Pair クラスを作成
     class Pair {
         int y, x;
         Pair(int y,int x) {
@@ -15,7 +22,9 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int m = scan.nextInt();
+        // 入力された迷路を格納する field の作成
         int[][] field = new int[m][];
+        // そのマスに来たターン数を格納する ans の作成
         int[][] ans = new int[m][];
         int sy = 0, sx = 0; // スタート座標
         int gy = 0, gx = 0; // ゴール座標
@@ -24,18 +33,15 @@ public class Main {
             ans[i] = new int[n];
             char[] data = scan.next().toCharArray();
             for (int j = 0; j < n; j++) {
-                if (data[j] == '#') {
-                    field[i][j] = 1;
-                } else if (data[j] == '.') {
-                    field[i][j] = 0;
-                } else if (data[j] == 'S') {
+                field[i][j] = data[j];
+                // Start と Gaul の座標を格納する
+                if (data[j] == 'S') {
                     sy = i;
                     sx = j;
-                    field[i][j] = 0;
-                } else {
+                }
+                if (data[j] == 'G') {
                     gy = i;
                     gx = j;
-                    field[i][j] = 0;
                 }
             }
         }
@@ -54,25 +60,26 @@ public class Main {
         que.add(new Pair(sy, sx));
 
         while (!que.isEmpty()) {
-            Pair pair = que.poll();
-            int y = pair.y;
-            int x = pair.x;
-            if (y == gy && x == gx) {
-                System.out.println(ans[y][x]);
+            Pair p = que.poll();
+            // ゴール座標ならばターン数を出力して終了
+            if (p.y == gy && p.x == gx) {
+                System.out.println(ans[p.y][p.x]);
                 break;
             }
             int[] dx = { 1, 0, -1,  0 };
             int[] dy = { 0, 1,  0, -1 };
             for (int i = 0; i < 4; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
+                int ny = p.y + dy[i], nx = p.x + dx[i];
+                // マップの範囲外ならばスキップ
                 if (ny < 0 || nx < 0 || ny >= m || nx >= n)
                     continue;
+                // すでに来たことあるマスならばスキップ
                 if (ans[ny][nx] != 0)
                     continue;
-                if (field[ny][nx] != 0)
+                // field のマスが壁ならばスキップ
+                if (field[ny][nx] == '#')
                     continue;
-                ans[ny][nx] = ans[y][x] + 1;
+                ans[ny][nx] = ans[p.y][p.x] + 1;
                 que.add(new Pair(ny, nx));
             }
         }
